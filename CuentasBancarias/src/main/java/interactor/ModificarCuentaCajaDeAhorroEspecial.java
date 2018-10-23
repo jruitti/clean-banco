1c7e7cd6bc3cc598777d99ab52b55d80d12ef5b4
Unknown <ivanfloresezequiel@gmail.com>
2018-10-23 19:35:08 -0300

    ModificarCuentaCajaDeAhorroEspecial

--- CuentasBancarias/src/main/java/interactor/ModificarCuentaCajaDeAhorroEspecial.java
+++ CuentasBancarias/src/main/java/interactor/ModificarCuentaCajaDeAhorroEspecial.java
@@ -1,5 +1,20 @@
 package interactor;
 
-public class ModificarCuentaCajaDeAhorroEspecial {
+import modelo.Cliente;
+import modelo.CuentaCajaDeAhorroEspecial;
+import repositorio.ICuentaCajaDeAhoorroEspecialRepositorio;
 
+
+public class ModificarCuentaCajaDeAhorroEspecial {
+	private ICuentaCajaDeAhoorroEspecialRepositorio elRepositorio;
+
+	public ModificarCuentaCajaDeAhorroEspecial(ICuentaCajaDeAhoorroEspecialRepositorio elRepositorio) {
+		this.elRepositorio = elRepositorio;
+	}
+
+	public boolean crearCuentaCajaAhorroEspecial(CuentaCajaDeAhorroEspecial cuenta1) {
+		CuentaCajaDeAhorroEspecial nuevaCuenta= new CuentaCajaDeAhorroEspecial(cuenta1.getNumero(),cuenta1.getTitular(),cuenta1.getSaldo());
+		//Cliente nuevoCliente=Cliente.instancia(elCliente.getIdCliente(), elCliente.getApellido(), elCliente.getNombre(), elCliente.getCuil(), elCliente.getDireccion());
+		return elRepositorio.guardarCajaAhorroEspecial(nuevaCuenta);	
+	}
 }
--- CuentasBancarias/src/main/java/repositorio/ICuentaCajaDeAhorroEspecialRepositorio.java
+++ CuentasBancarias/src/main/java/repositorio/ICuentaCajaDeAhorroEspecialRepositorio.java
@@ -1,10 +1,11 @@
 package repositorio;
 
-import modelo.CuentaCajaDeAhorroComun;
+import modelo.CuentaCajaAhorro;
 import modelo.CuentaCajaDeAhorroEspecial;
 
 public interface ICuentaCajaDeAhorroEspecialRepositorio {
 	public boolean crearCuentaCajaDeAhorroEspecial(CuentaCajaDeAhorroEspecial persCuentaCajaDeAhorroEspecial);
-	public boolean modificarCuentaCajaDeAhorroEspecial(CuentaCajaDeAhorroComun persCuentaCajaDeAhorroEspecial);
+	public boolean modificarCuentaCajaDeAhorroEspecial(CuentaCajaAhorro persCuentaCajaDeAhorroEspecial);
+	public boolean guardarCajaAhorroEspecial(CuentaCajaDeAhorroEspecial nuevaCuenta);
 	
 }
