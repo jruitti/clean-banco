fae6c7cb1b9a302f1126646517e1d92471009cdc
Dardo <dardomorelli@gmail.com>
2018-10-31 19:01:59 -0300

    Unit Test Modelo Caja de ahorro Especial

---
+++ CuentasBancarias/src/test/java/modeloUnitTest/CuentaCajaDeAhorroEspecialUnitTest.java
@@ -0,0 +1,40 @@
+package modeloUnitTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import modelo.Cliente;
+import modelo.CuentaCajaDeAhorroEspecial;
+class CuentaCajaDeAhorroEspecialUnitTest {
+
+
+	@Test
+	public void intaciarCuentaCajaDeAhorroEspecial_CuentaCompleta_IntanciaCuentaCajaDeAhorroEspecial() {
+		CuentaCajaDeAhorroEspecial cuentaCajaDeAhorroEspecial;
+		Cliente clienteCuenta = Cliente.instancia(2, "Perez", "Pepe", "12345", "Santa Rosa 321");
+		cuentaCajaDeAhorroEspecial = CuentaCajaDeAhorroEspecial.instancia("123456789", clienteCuenta);
+		assertNotNull(cuentaCajaDeAhorroEspecial);
+	}
+
+	@Test
+	public void intaciarCuentaCajaDeAhorroEspecial_CuentaNoCompleta_IntanciaCuentaCajaDeAhorroEspecial() {
+		CuentaCajaDeAhorroEspecial cuentaCajaDeAhorroEspecial;
+		cuentaCajaDeAhorroEspecial = CuentaCajaDeAhorroEspecial.instancia("123456789", null);
+		assertNull(cuentaCajaDeAhorroEspecial);
+	}
+
+	@Test
+	public void intaciarCuentaCajaAhorro_CuentaSinNuemero_IntanciaCuentaCajaDeAhorroEspecial() {
+		CuentaCajaDeAhorroEspecial cuentaCajaDeAhorroEspecial;
+		Cliente clienteCuenta = Cliente.instancia(2, "Perez", "Pepe", "12345", "Santa Rosa 321");
+		cuentaCajaDeAhorroEspecial = CuentaCajaDeAhorroEspecial.instancia(null, clienteCuenta);
+		assertNull(cuentaCajaDeAhorroEspecial);
+	}
+
+	@Test
+	public void intaciarCuentaCajaDeAhorroEspecial_CuentaSinDatos_IntanciaCuentaCajaDeAhorroEspecial() {
+		CuentaCajaDeAhorroEspecial cuentaCajaDeAhorroEspecial = CuentaCajaDeAhorroEspecial.instancia(null, null);
+		assertNull(cuentaCajaDeAhorroEspecial);
+	}
+
+}
