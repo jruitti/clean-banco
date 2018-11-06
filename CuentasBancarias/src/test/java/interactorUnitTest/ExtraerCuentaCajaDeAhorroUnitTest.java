c3c8aa5a14062e231dfc633a493883b46887661d
Dardo <dardomorelli@gmail.com>
2018-11-06 18:29:47 -0300

    test Extraer Cuanta Caja de ahorro

---
+++ CuentasBancarias/src/test/java/interactorUnitTest/ExtraerCuentaCajaDeAhorroUnitTest.java
@@ -0,0 +1,14 @@
+package interactorUnitTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ExtraerCuentaCajaDeAhorroUnitTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ CuentasBancarias/src/test/java/modeloUnitTest/ExtraerCuentaCajaDeAhorroUnitTest.java
@@ -0,0 +1,14 @@
+package modeloUnitTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ExtraerCuentaCajaDeAhorroUnitTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
