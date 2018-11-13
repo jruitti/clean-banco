46cf5b54726cc99baa8c645efb7c6b9a0b5cfa58
Luciano <lucianoalive99@gmail.com>
2018-11-12 22:18:48 -0300

    consultarClienteUnitTest
    me olvide subirlo, perdon

---
+++ CuentasBancarias/src/test/java/interactorUnitTest/ConsultarClienteUnitTest.java
@@ -0,0 +1,42 @@
+package interactorUnitTest;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import interactor.consultarClienteUseCase;
+import modelo.Cliente;
+
+public class ConsultarClienteUnitTest {
+	@Test
+	public void consultarCliente_ClienteExiste_ClienteDevuelto() {
+		RepositorioCliente elRepositorio = new RepositorioCliente();
+		elRepositorio.existeCliente=true;
+		
+		Cliente elCliente=Cliente.instancia(1,"Perez","Juan","20-12345678","San Martin 100");
+		
+		consultarClienteUseCase consultarClienteUseCase = new consultarClienteUseCase(elRepositorio);
+		
+		boolean resultado  = consultarClienteUseCase.ConsultarCliente(elCliente);
+		
+		assertTrue(resultado);
+	}
+	
+	@Test
+	public void consultarCliente_ClienteNoExiste_ClienteNoDevuelto() {
+		RepositorioCliente elRepositorio = new RepositorioCliente();
+		elRepositorio.clientePorCuil = false;
+		
+		Cliente elCliente=Cliente.instancia(1,"Perez","Juan","20-12345678","San Martin 100");
+		
+		consultarClienteUseCase consultarClienteUseCase = new consultarClienteUseCase(elRepositorio);
+		
+		boolean resultado  = consultarClienteUseCase.ConsultarCliente(elCliente);
+		
+		assertFalse(resultado);
+	}
+}
+	
+
+
